dacaa9180c21300ffcdadb6fbe192ca72638b658
Mohamed Ben Rhaiem <mohamed.ben.rhaiem.73@gmail.com>
2023-04-19 11:31:07 +0200

    Setting the theme for Dark/Light mode usage & adding the top bar & the cover image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import "./App.css";
-
+import TopBar from "./scenes/global/TopBar";
+import Box from "@mui/material/Box";
+import { ColorModeContext,useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
+  const [theme , colorMode]= useMode()
   return (
-    <>
-      <p>hello world!</p>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Box
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL + './assets/coverImage_1.jpg'})`,
+        backgroundSize:'cover',
+        backgroundRepeat:'no-repeat',
+        height: '100vh',
+        backgroundPosition: 'center',
+        '@media (max-width: 768px)': {
+          backgroundSize: 'contain',
+          backgroundPosition: 'top',
+        },
+      }}
+    >
+        <div className="app">
+          <main className="content">
+            <TopBar/>
+          </main>
+        </div>
+        </Box>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
