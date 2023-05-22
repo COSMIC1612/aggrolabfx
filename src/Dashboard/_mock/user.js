1a60a53b69ba273164cee7b444843f79bdb53d04
Mohamed Ben Rhaiem <mohamed.ben.rhaiem.73@gmail.com>
2023-05-22 21:53:29 +0200

    Some changes in the dashboard

--- src/Dashboard/_mock/user.js
+++ src/Dashboard/_mock/user.js
@@ -1,110 +1,91 @@
-import { faker } from '@faker-js/faker';
-/* import { sample } from 'lodash'; */
+
+
 
 // ----------------------------------------------------------------------
 const users = [{
-  id: faker.datatype.uuid(),
+  id: 1,
   name:'put',
   company:'EUR/USD',
   role:'1.0818',
   isVerified:'0.0005',
   status:'05-16-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 2,
   name:'call',
   company:'EUR/USD',
   role:'1.0818',
   isVerified:'0.0153',
   status:'05-16-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 3,
   name:'put',
   company:'EUR/USD',
   role:'1.0769',
   isVerified:'0.0007',
   status:'05-23-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 4,
   name:'call',
   company:'EUR/USD',
   role:'1.0769',
   isVerified:'0.02073',
   status:'05-23-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 5,
   name:'put',
   company:'EUR/USD',
   role:'1.0679',
   isVerified:'0.0011',
   status:'06-08-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 6,
   name:'call',
   company:'EUR/USD',
   role:'1.0679',
   isVerified:'0.03099',
   status:'06-08-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 7,
   name:'put',
   company:'EUR/USD',
   role:'1.0551',
   isVerified:'0.00168',
   status:'07-07-2023',
 },{
-  id: faker.datatype.uuid(),
+  id:8,
   name:'call',
   company:'EUR/USD',
   role:'1.0551',
   isVerified:'0.04598',
   status:'07-07-2023',
 },{
-  id: faker.datatype.uuid(),
+  id:9,
   name:'put',
   company:'EUR/USD',
   role:'1.0447',
   isVerified:'0.00218',
   status:'09-08-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 10,
   name:'call',
   company:'EUR/USD',
   role:'1.0447',
   isVerified:'0.05838',
   status:'09-08-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 11,
   name:'put',
   company:'EUR/USD',
   role:'1.0232',
   isVerified:'0.00328',
   status:'11-09-2023',
 },{
-  id: faker.datatype.uuid(),
+  id: 12,
   name:'put',
   company:'EUR/USD',
   role:'1.0672',
   isVerified:'0.00953',
   status:'11-09-2023',
 }]
-/* const users = [...Array(24)].map((_, index) => ({
-  id: faker.datatype.uuid(),
-  name: sample(['put','call']),
-  company: faker.company.name(),
-  isVerified: faker.datatype.boolean(),
-  status: sample(['active', 'banned']),
-  role: sample([
-    'Leader',
-    'Hr Manager',
-    'UI Designer',
-    'UX Designer',
-    'UI/UX Designer',
-    'Project Manager',
-    'Backend Developer',
-    'Full Stack Designer',
-    'Front End Developer',
-    'Full Stack Developer',
-  ]),
-}));
- */
+
 export default users;
--- src/Dashboard/routes.js
+++ src/Dashboard/routes.js
@@ -3,11 +3,9 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import SimpleLayout from './layouts/simple';
 //
-import BlogPage from './pages/BlogPage';
+
 import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
-import Page404 from './pages/Page404';
-import ProductsPage from './pages/ProductsPage';
+
 import DashboardAppPage from './pages/DashboardAppPage';
 import Simulation from "./pages/Simulation";
 
@@ -23,19 +21,14 @@ export default function Router() {
         { path: 'app', element: <DashboardAppPage /> },
         { path: 'options', element: <UserPage /> },
         { path: 'simulation', element: <Simulation /> },
-        { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
+       
       ],
     },
-    {
-      path: 'login',
-      element: <LoginPage />,
-    },
+    
     {
       element: <SimpleLayout />,
       children: [
         { element: <Navigate to="/dashboard/app" />, index: true },
-        { path: '/dashboard', element: <Page404 /> },
         { path: '/dashboard', element: <Navigate to="/dashboard" /> },
       ],
     },
